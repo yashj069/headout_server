405c0fd7554d6ef8e2759e75402d5a448ee03c57
prateek <prateekjakhar123@gmail.com>
2025-04-30 16:52:44 +0530

    Removed unnessary code

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,9 @@
 import { Router } from "express";
-// import dataRoutes from "./dataRoutes.js";
-// import submitRoutes from "./submitRoutes.js";
 import userRoutes from "./userRoutes.js";
 import gameRoutes from "./gameRoutes.js";
 
 const router = Router();
 
-// router.use("/data", dataRoutes);
-// router.use("/submit", submitRoutes);
 router.use("/user", userRoutes);
 router.use("/game", gameRoutes);
 
--- services/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-// import { isProduction } from './util.js';
-
-mongoose.set("debug", false);
-
-export const connectMongoDB = async () => {
-  await mongoose.connect(process.env.MONGO_URI);
-  console.log(`Mongodb is connected`);
-};
